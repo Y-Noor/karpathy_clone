8d1e3e71a28d001a48595af05802d604e2b37d6b
Y-Noor <59338864+Y-Noor@users.noreply.github.com>
2024-02-23 01:36:20 +0400

    Add files via upload

---
+++ digitrecog.py
@@ -0,0 +1,65 @@
+from pandas._libs.hashtable import mode
+from engine import Value
+from nn import Neuron, Layer, MLP
+import random 
+import numpy as np
+import pandas as pd
+
+np.random.seed(1337)
+random.seed(1337)
+
+df = pd.read_csv('cw2DataSet1.csv')
+
+X = df.iloc[:,:-1].astype(float)
+y = df.iloc[:, -1].astype(float)
+
+model = MLP(64, [16, 16, 10])
+print(model)
+print("number of parameters", len(model.parameters()))
+
+def loss(batch_size=None):
+    
+    # inline DataLoader :)
+    if batch_size is None:
+        Xb, yb = X, y
+    else:
+        ri = np.random.permutation(X.shape[0])[:batch_size]
+        Xb, yb = X[ri], y[ri]
+    inputs = [list(map(Value, xrow)) for xrow in Xb]
+    
+    # forward the model to get scores
+    scores = list(map(model, inputs))
+    
+    # svm "max-margin" loss
+    losses = [(1 + -yi*scorei).relu() for yi, scorei in zip(yb, scores)]
+    data_loss = sum(losses) * (1.0 / len(losses))
+    # L2 regularization
+    alpha = 1e-4
+    reg_loss = alpha * sum((p*p for p in model.parameters()))
+    total_loss = data_loss + reg_loss
+    
+    # also get accuracy
+    accuracy = [(yi > 0) == (scorei.data > 0) for yi, scorei in zip(yb, scores)]
+    return total_loss, sum(accuracy) / len(accuracy)
+
+total_loss, acc = loss()
+print(total_loss, acc)
+
+
+# optimization
+for k in range(100):
+    
+    # forward
+    total_loss, acc = loss()
+    
+    # backward
+    model.zero_grad()
+    total_loss.backward()
+    
+    # update (sgd)
+    learning_rate = 1.0 - 0.9*k/100
+    for p in model.parameters():
+        p.data -= learning_rate * p.grad
+    
+    if k % 1 == 0:
+        print(f"step {k} loss {total_loss.data}, accuracy {acc*100}%")
